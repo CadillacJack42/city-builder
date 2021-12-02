9b9fb597ffc368c5f2f035eb997a4aed9d04a0b9
James Keicher <jamesdkeicher@gmail.com>
2021-12-02 15:38:17 -0800

    Completed city builder game

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const house = document.getElementById('house');
 const sloganInput = document.getElementById('slogan-input');
 const btn = document.getElementById('btn');
 
+const list = document.getElementById('list');
+
+const count = document.getElementById('count');
+
 // let state
 let state = {
     slogans: [],
@@ -19,22 +23,53 @@ let state = {
 
 // event.target = dom
 // event.target.id = city/water/house
-// event.target.value = what city
+// event.target.value = what city which water which house
 city.addEventListener('change', (event) => {
     // console.log('Hello City');
-    handleClick(event.target.id)
+    handleClick(event);
 });
-water.addEventListener('change', () => {
-    console.log('Hello water');
+water.addEventListener('change', (event) => {
+    // console.log('Hello water');
+    handleClick(event);
 });
-house.addEventListener('change', () => {
-    console.log('Hello house');
+house.addEventListener('change', (event) => {
+    // console.log('Hello house');
+    handleClick(event);
 });
 
 btn.addEventListener('click', () => {
-    console.log(sloganInput.value); 
+    displaySlogans();
 });
 
 const handleClick = (e) => {
-  console.log(e);
+    state[e.target.id]++;
+    console.log(state);
+    let where = document.getElementById(`${e.target.id}-img`);
+    where.style.backgroundImage = `url('./assets/${e.target.value}.jpeg')`;
+    displayStats();
+  // console.log(e);
 };
+
+const displayStats = () => {
+    count.textContent = '';
+    let p = document.createElement('p');
+    p.textContent = createCountString(state.city, state.water, state.house);
+    count.append(p);
+};
+
+const displaySlogans = () => {
+    state.slogans.push(sloganInput.value);
+    console.log(state.slogans);
+    sloganInput.value = '';
+    list.textContent = '';
+    for (const slogan of state.slogans) {
+        let p = document.createElement('p');
+        p.textContent = slogan;
+        list.append(p);
+    }
+    
+};
+
+
+const enter = document.getElementById('slogan-headline');
+enter.style.margin = 0;
